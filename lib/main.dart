805f0958600995081f906e75ac3c8d7f9d78e05d
Ahmed Mohsen <105174620+AhmedMohsen2492@users.noreply.github.com>
2022-10-18 00:42:54 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:news_app/layout/news_app/cubit/cubit.dart';
+import 'package:news_app/layout/news_app/cubit/states.dart';
+import 'package:news_app/layout/news_app/news_layout.dart';
+import 'package:news_app/shared/bloc_observer.dart';
+import 'package:news_app/shared/network/remote/dio_helper.dart';
+import 'shared/network/local/cache_helper.dart';
+
+void main() async
+{
+  //بيتاكد ان كل حاجة هنا فى الميثود خلصت و بعدين يفتح الابليكيشن
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  DioHelper.init();
+  await CacheHelper.init();
+  bool? isDark = CacheHelper.getData(key:'isDark') ;
+  isDark ??= false ;
+  runApp(Myapp(isDark));
+}
+
+class Myapp extends StatelessWidget
+{
+  final bool isDark ;
+  Myapp(this.isDark);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => NewsCubit()..getBusiness()..getSports()..getScience(),),
+        BlocProvider(create: (BuildContext context) => NewsCubit()..changeAppMode(fromShared: isDark,),),
+      ],
+      child: BlocConsumer<NewsCubit,NewsStates>(
+        listener: (context, state)  {},
+        builder: (context,state)
+        {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.deepOrange,
+              scaffoldBackgroundColor: Colors.white,
+              appBarTheme: AppBarTheme(
+                titleSpacing: 20.0,
+                backwardsCompatibility: false,
+                systemOverlayStyle: SystemUiOverlayStyle(
+                  statusBarColor: Colors.white,
+                  statusBarIconBrightness: Brightness.dark,
+                ),
+                backgroundColor: Colors.white,
+                elevation: 0.0,
+                iconTheme: IconThemeData(
+                  color: Colors.black,
+                ),
+                titleTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ) ,
+              bottomNavigationBarTheme: BottomNavigationBarThemeData(
+                type: BottomNavigationBarType.fixed,
+                selectedItemColor: Colors.deepOrange,
+                unselectedItemColor: Colors.grey,
+                elevation: 20.0,
+                backgroundColor:Colors.white ,
+              ),
+              textTheme: TextTheme(
+                bodyText1: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+            ) ,
+            darkTheme: ThemeData(
+              scaffoldBackgroundColor: HexColor('333739'),
+              primarySwatch: Colors.deepOrange,
+              appBarTheme: AppBarTheme(
+                titleSpacing: 20.0,
+                backwardsCompatibility: false,
+                systemOverlayStyle: SystemUiOverlayStyle(
+                  statusBarColor: HexColor('333739'),
+                  statusBarIconBrightness: Brightness.light,
+                ),
+                backgroundColor: HexColor('333739'),
+                elevation: 0.0,
+                iconTheme: IconThemeData(
+                  color: Colors.white,
+                ),
+                titleTextStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ) ,
+              bottomNavigationBarTheme: BottomNavigationBarThemeData(
+                type: BottomNavigationBarType.fixed,
+                selectedItemColor: Colors.deepOrange,
+                unselectedItemColor: Colors.grey,
+                elevation: 20.0,
+                backgroundColor:HexColor('333739') ,
+              ),
+              textTheme: TextTheme(
+                bodyText1: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white
+                ),
+              ),
+            ),
+            themeMode: NewsCubit.get(context).isDark ?ThemeMode.dark : ThemeMode.light,
+            home: Directionality(
+                textDirection: TextDirection.ltr,
+                child: NewsLayout()),
+          );
+        },
+      ),
+    );
+  }
+}
